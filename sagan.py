9bb23094cda11b413aaaea06656c5bfdd8432d26
rsn870 <50112766+rsn870@users.noreply.github.com>
2020-05-10 15:16:35 +0530

    Update sagan.py

--- sagan.py
+++ sagan.py
@@ -216,13 +216,13 @@ class Discriminator(nn.Module):
 
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
+#def weights_init(m):
+    #classname = m.__class__.__name__
+    #if classname.find('Conv') != -1:
+        #m.weight.data.normal_(0.0, 0.02)
+    #elif classname.find('BatchNorm') != -1:
+        #m.weight.data.normal_(1.0, 0.02)
+        #m.bias.data.fill_(0)
 
 netG = Generator()
 netG = netG.to(device)
